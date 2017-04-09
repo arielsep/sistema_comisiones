be6e5faf3034b5f3ffa9d1ba5a32b5741c8e8690
Annelisse Emeterio <Annelisseet8@gmail.com>
2017-04-08 22:38:20 -0400

    Agrega clase conexion

---
+++ src/main/java/edu/itla/sistemacomisiones/database/Conexion.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.itla.sistemacomisiones.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Annelisse
+ */
+public class Conexion {
+    
+    private static Conexion con;
+    private Connection conn;
+    
+    public static Conexion getInstancia(){
+        if (con == null ){
+            try {
+                con = new Conexion();
+            } catch (SQLException ex) {
+                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return con;
+    }
+
+    
+    private Conexion() throws SQLException, ClassNotFoundException{
+        Class.forName("com.mysql.jdbc.Driver");  
+        conn = DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/sistema_comision","usuario_sistemas_comisiones","123456");  
+    }
+}
