0720fbf2e52936f449b420fe24c92c71282d0e75
Ariel Porfirio Sepulveda Rodriguez <arielporfirio@gmail.com>
2017-04-10 01:32:27 -0500

    Agregar modelo persona

---
+++ src/main/java/edu/itla/sistemacomisiones/database/model/Persona.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.itla.sistemacomisiones.database.model;
+
+/**
+ *
+ * @author ariel
+ */
+public class Persona {
+   private int id;
+   private String nombre;
+   private String apellido;
+   private String documentoIdentidad;       
+   private char sexo;
+   private String correo;
+   private String celular;
+   private Direccion direccion; 
+
+    
+
+    public Persona() {
+    }
+
+    public Persona(int id, String nombre, String apellido, String documentoIdentidad, char sexo, String correo, String celular, Direccion direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.documentoIdentidad = documentoIdentidad;
+        this.sexo = sexo;
+        this.correo = correo;
+        this.celular = celular;
+        this.direccion = direccion;
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+   
+       
+}
+
+
