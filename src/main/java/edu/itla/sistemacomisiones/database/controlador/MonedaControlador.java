77907807443215b7dc9ea549ef4933a2fe4349c7
wilman cepeda <wilmancepeda@hotmail.com>
2017-04-10 01:55:18 -0400

    Agrega controlador de moneda

---
+++ src/main/java/edu/itla/sistemacomisiones/database/controlador/MonedaControlador.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.itla.sistemacomisiones.database.controlador;
+
+import edu.itla.sistemacomisiones.database.model.Direccion;
+import edu.itla.sistemacomisiones.database.model.Moneda;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author wilmanpc
+ */
+public class MonedaControlador extends Controlador<Moneda>  {
+    private static MonedaControlador controlador;
+    
+    public static MonedaControlador getInstancia(){
+           if (controlador == null){
+               controlador = new MonedaControlador();
+           }
+           return controlador;
+    }
+    
+     private MonedaControlador() {
+        super("moneda");
+    }
+     
+    @Override
+    public Moneda crear(Moneda obj) {
+    try {
+            PreparedStatement st = con.prepareStatement("INSERT INTO "+ tablaBaseDeDatos +
+                    "  (`nombre`, `simbolo`, `tasa`, `es_principal`) VALUES (?, ?, ?, ?); " );
+            st.setString(1, obj.getNombre());
+            st.setString(2, obj.getSimbolo());
+            st.setDouble(3, obj.getTasa());
+            st.setBoolean(4,obj.isEsPrincipal());
+            obj.setId(st.executeUpdate());
+ 
+        } catch (SQLException ex) {
+            Logger.getLogger(Controlador.class.getName()).log(Level.SEVERE,"crear" , ex);
+        }
+        return obj; 
+    }
+
+    @Override
+    public Moneda actualizar(Moneda obj) {
+       try {
+            PreparedStatement st = con.prepareStatement("UPDATE "+ tablaBaseDeDatos +
+                    " SET `nombre`=?, `simbolo`=?, `tasa`=?, `es_principal`=? WHERE `id`=?;" );
+            st.setString(1, obj.getNombre());
+            st.setString(2, obj.getSimbolo());
+            st.setDouble(3, obj.getTasa());
+            st.setBoolean(4, obj.isEsPrincipal());
+            st.setInt(5, obj.getId());
+            st.executeUpdate();
+ 
+        } catch (SQLException ex) {
+            Logger.getLogger(Controlador.class.getName()).log(Level.SEVERE,"actulizar" , ex);
+        }
+        return obj;
+    }
+
+    @Override
+    public Moneda buscar(String term) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Moneda crearDeResultSet(ResultSet rs) throws SQLException {
+       return new Moneda(rs.getInt("id"), rs.getString("nombre"),
+                    rs.getString("simbolo"),rs.getDouble("tasa"),rs.getBoolean("esPrincipal"));
+    }
+    
+}
