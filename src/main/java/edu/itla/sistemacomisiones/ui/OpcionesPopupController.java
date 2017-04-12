64fb22d434907adbfffc9acd7528d931f7a0f604
Ariel Porfirio Sepulveda Rodriguez <arielporfirio@gmail.com>
2017-04-12 13:51:11 -0400

    Agrega opciones a la ventana principal

---
+++ src/main/java/edu/itla/sistemacomisiones/ui/OpcionesPopupController.java
@@ -0,0 +1,39 @@
+package edu.itla.sistemacomisiones.ui;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import com.jfoenix.controls.JFXListView;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author ariel
+ */
+public class OpcionesPopupController implements Initializable {
+
+    @FXML
+    private JFXListView<?> toolbarPopupList;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void submit(MouseEvent event) {
+    }
+    
+}
